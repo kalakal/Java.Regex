c37e4b781da8d9aee2f6694e3217bb38350bc0d3
kalakal <sayedfaizanqadri@gmail.com>
2023-03-17 20:24:40 +0530

    Added Problem for Password validation Rule 2

--- src/com/bridgelab/user_registration/UserRegistrationRegex.java
+++ src/com/bridgelab/user_registration/UserRegistrationRegex.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-// UC3-Method for validation of Email.
+// UC6-Method for validation of Password with rule -2.
 public class UserRegistrationRegex {
 
     public static void firstName(String firstName) {
@@ -17,6 +17,7 @@ public class UserRegistrationRegex {
             System.out.println(firstName + " : is a invalid first name ");
         }
     }
+
     public static void lastName(String lastName) {
         String regex = "^[A-Z]{1}[a-zA-Z]{2,}$";
         Pattern p = Pattern.compile(regex);
@@ -27,6 +28,7 @@ public class UserRegistrationRegex {
             System.out.println(lastName + " : is a invalid last name ");
         }
     }
+
     public static void email(String email) {
         String regex = "^[a-z]{3,}[.+-_]*[a-z0-9]*{3,}[@][a-z0-9]{1,}[.][a-z]{2,}[.]*[a-z]*{2,}$";
         Pattern p = Pattern.compile(regex);
@@ -37,6 +39,7 @@ public class UserRegistrationRegex {
             System.out.println(email + " : is a invalid email ");
         }
     }
+
     public static void mobileNumber(String mobile) {
         String regex = "^[0-9]{2}[0-9]{10}$";
         Pattern p = Pattern.compile(regex);
@@ -48,12 +51,24 @@ public class UserRegistrationRegex {
         }
     }
 
+    public static void password(String password) {
+        String regex = "(?=.*[A-Z])[A-za-z0-9@_#$%!]{8,}";
+        Pattern p = Pattern.compile(regex);
+        Matcher match = p.matcher(password);
+        if (match.matches()) {
+            System.out.println(" Password is valid ");
+        } else {
+            System.out.println(" Password is invalid ");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome in User registration program using Regex");
         Scanner sc = new Scanner(System.in);
-        System.out.println("What do want enter \n1. First Name \n2. Last Name \n3. E-mail \n4. Mobile number");
+        System.out.println(
+                "What do want enter \n1. First Name \n2. Last Name \n3. E-mail \n4. Mobile number \n5. Password ");
         int option = sc.nextInt();
-        switch(option) {
+        switch (option) {
 
             case 1:
                 System.out.println("Enter First Name");
@@ -75,6 +90,11 @@ public class UserRegistrationRegex {
                 System.out.println("Enter Mobile Number");
                 String mobile = sc.next();
                 mobileNumber(mobile);
+                break;
+            case 5:
+                System.out.println("Enter Password ");
+                String password = sc.next();
+                password(password);
                 break;
         }
     }
