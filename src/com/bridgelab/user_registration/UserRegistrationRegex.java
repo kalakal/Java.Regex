b79daac28da0cebdc0b4741f4de487ccc66ec8c7
kalakal <sayedfaizanqadri@gmail.com>
2023-03-17 23:06:38 +0530

    Added Problem for Password validation Rule 4

--- src/com/bridgelab/user_registration/UserRegistrationRegex.java
+++ src/com/bridgelab/user_registration/UserRegistrationRegex.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-// UC7-Method for validation of Password with rule -3.
+// UC8-Method for validation of Password with rule -4.
 public class UserRegistrationRegex {
 
     public static void firstName(String firstName) {
@@ -52,7 +52,7 @@ public class UserRegistrationRegex {
     }
 
     public static void password(String password) {
-        String regex = "(?=.*[A-Z])(?=.*[0-9])[A-za-z0-9@_#$%!]{8,}";
+        String regex = "(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=[^@#$%^&+=]*[@#$%^&+=][^@#$%^&+=]*$).{8,}";
         Pattern p = Pattern.compile(regex);
         Matcher match = p.matcher(password);
         if (match.matches()) {
